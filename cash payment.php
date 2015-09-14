38045414541026b53a7969e01e4714dd9f05a222
mosasad <m.asadollahy@yahoo.com>
2015-09-14 13:51:26 +0430

    قرار داد

--- cash payment.php
+++ cash payment.php
@@ -1,25 +1,91 @@
 <!DOCTYPE html>
-    <html>
-<meta charset="UTF-8">
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <script type="text/javascript">
+        function showUser(str) {
+            if (str == "") {
+                document.getElementById("name").innerHTML = "";
+                return;
+            } else {
+                if (window.XMLHttpRequest) {
+                    // code for IE7+, Firefox, Chrome, Opera, Safari
+                    xmlhttp = new XMLHttpRequest();
+                } else {
+                    // code for IE6, IE5
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                xmlhttp.onreadystatechange = function() {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        document.getElementById("name").innerHTML = xmlhttp.responseText;
+                    }
+                }
+                xmlhttp.open("GET","checkpeyment.php?q="+str,true);
+                xmlhttp.send();
+            }
+        }
+    </script>
+
+</head>
+
 <body>
-<form method="post" action="<?php echo($_SERVER["PHP_SELF"]); ?>">
-    سرمایه گذار ID:<input type="text" name="ID"><br>
-    اسم سرمایه گذار:<br>
-    مبلغ:<input type="text" name="cash"><br>
-    <input type="submit" name="submit" value="تایید">
-</form>
+<table>
+    <form method="post" action="<?php echo $_SERVER['php_self']?>">
+        <tr>
+            <td><label>ID خود را وارد کنید :</label></td>
+            <td><input type="text" name="ID" onchange="showUser(this.value)"></td>
+        </tr>
+        <tr>
+            <td><label>نام سرمایه گذار</label></td>
+            <td><label id="name"></label></td>
+        </tr>
+        <tr>
+            <td><label>مبلغ :</label></td>
+            <td><input type="text" name="cash"> </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" name="submit" value="تایید"></td>
+        </tr>
+
+    </form>
+</table>
+
+</body>
+</html>
+
 <?php
+$conn = oci_connect("system", "data1111224", "192.168.137.15:1521/GENERAL");
+if (!$conn) {
+    echo "connection failed";
+}
+$id=$_GET['q'];
+$sql="select T1_2,T1_3 from T1 WHERE T1_1='$id'";
+$result=oci_parse($conn,$sql);
+if(oci_fetch_assoc($result)>0){
+    $row=oci_fetch_assoc($result);
+    echo $row['T1_2'],$row['T1_3'];
+}
+
+
 if(isset($_POST['submit'])) {
-    $conn = oci_connect("system", "data1111224", "192.168.137.15:1521/GENERAL");
-    if (!$conn) {
-        echo "connection failed";
-    }
+
     $ID=$_POST['ID'];
     $cash=$_POST['cash'];
-    $execute=oci_parse($conn,"UPDATE T4 SET T4_5='$cash' WHERE T4_1='$ID'");
+    $execute=oci_parse($conn,"insert into T4 (T4_1,T4_4) VALUE ('$ID',DATE )");
+    oci_execute($execute);
+
+    $execute=oci_parse($conn,"select T4_3 from T4 WHERE T4_1='$ID'");
+    $rows=oci_fetch_assoc($execute);
+    $contractID=$rows['T4_3'];
+    oci_execute($execute);
+
+    $execute=oci_parse($conn,"insert into T29 (T29_1,T29_2,T29_3) VALUE ('$contractID','1','کد رهگیری')");
+    oci_execute($execute);
+
+    $execute=oci_parse($conn,"insert into T14 (T14_1, T14_2,T14_5) VALUE ('کد رهگیری','$cash',DATe )");
     oci_execute($execute);
     oci_close($conn);
 }
 ?>
-</body>
-    </html>
